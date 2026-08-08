f4656126b6354cd14f92b920c6fb5373d7ad9ed8
viniciusdutra314 <dutravinisousa2@usp.br>
2026-08-08 20:45:44 -0300

    feat: ex01 quase finalizado

--- ex-01/main.py
+++ ex-01/main.py
@@ -1,22 +1,16 @@
 import random
 from dataclasses import dataclass
+from itertools import batched
 from typing import Literal
 
 type Matriz[T] = list[list[T]]
 type Vetor[T] = list[T]
-type Binário = Literal[+1, -1]
-type Classificacao = Binário
+type Pixel = Literal[+1, -1]
+type Classificação = Literal[+1, -1]
 
-X: Binário = +1
-_: Binário = -1
-MAX_TENTATIVAS = 10000
-GERADOR = random.Random(123456789)
-PROB_FLIP = 2 / 25
-NUM_EXEMPLOS_CLASSIFICACAO = 10
-TAXA_APRENDIZADO = 0.01
-
-
-EXEMPLOS_PUROS_Y: list[tuple[Classificacao, Matriz[Binário]]] = [
+X: Pixel = +1
+_: Pixel = -1
+EXEMPLOS_PUROS_Y: list[tuple[Classificação, Matriz[Pixel]]] = [
     (
         +1,
         [
@@ -40,21 +34,44 @@ EXEMPLOS_PUROS_Y: list[tuple[Classificacao, Matriz[Binário]]] = [
 ]
 
 
-EXEMPLOS_Y: list[tuple[Classificacao, Vetor[Binário]]] = [
-    (
-        classificação,
-        [
-            -valor if GERADOR.random() < PROB_FLIP else valor
-            for linha in matriz
-            for valor in linha
-        ],
-    )
-    for classificação, matriz in EXEMPLOS_PUROS_Y
-    for _ in range(NUM_EXEMPLOS_CLASSIFICACAO)
-]
+def gerar_exemplos_classificados(
+    num_exemplos_por_classificao: int, gerador: random.Random, prob_flip: float
+) -> list[tuple[Classificação, Vetor[Pixel]]]:
+    return [
+        (
+            classificação,
+            [
+                -valor if gerador.random() < prob_flip else valor
+                for linha in matriz
+                for valor in linha
+            ],
+        )
+        for classificação, matriz in EXEMPLOS_PUROS_Y
+        for _ in range(num_exemplos_por_classificao)
+    ]
 
 
-def sinal(x: float) -> Binário:
+def printar_imagem_e_classificação(
+    vector: Vetor[Pixel], classificacao: Classificação
+) -> None:
+    print(f"Classificação: {classificacao}")
+    for linha in range(5):
+        for coluna in range(5):
+            print("X" if vector[linha * 5 + coluna] == +1 else "_", end=" ")
+        print()
+
+
+def printar_alguns_exemplos(
+    exemplos: list[tuple[Classificação, Vetor[Pixel]]],
+    num_para_printar: int,
+    gerador: random.Random,
+) -> None:
+    exemplos_aleatorios = gerador.sample(exemplos, num_para_printar)
+    for classificacao, exemplo in exemplos_aleatorios:
+        printar_imagem_e_classificação(exemplo, classificacao)
+
+
+def sinal(x: float) -> Classificação:
     return +1 if x > 0 else -1
 
 
@@ -63,7 +80,7 @@ class Modelo:
     pesos: Vetor[float]
     vies: float
 
-    def calcular_z(self, entrada: Vetor[Binário]) -> float:
+    def calcular_z(self, entrada: Vetor[Pixel]) -> float:
         return (
             sum(
                 peso * val_entrada
@@ -72,42 +89,62 @@ class Modelo:
             + self.vies
         )
 
-    def inferir(self, entrada: Vetor[Binário]) -> Classificacao:
+    def inferir(self, entrada: Vetor[Pixel]) -> Classificação:
         return sinal(self.calcular_z(entrada))
 
     def aprender(
-        self, exemplos: list[tuple[Classificacao, Vetor[Binário]]]
+        self,
+        taxa_aprendizagem: float,
+        exemplos: list[tuple[Classificação, Vetor[Pixel]]],
     ) -> "Modelo":
         modelo = self
         for classificação, entrada in exemplos:
             z = modelo.calcular_z(entrada)
+            erro = z - classificação
+            ajuste = taxa_aprendizagem * erro
             modelo = Modelo(
                 pesos=[
-                    peso_original - TAXA_APRENDIZADO * (z - classificação) * x
+                    peso_original - ajuste * x
                     for peso_original, x in zip(modelo.pesos, entrada, strict=True)
                 ],
-                vies=modelo.vies - TAXA_APRENDIZADO * (z - classificação),
+                vies=modelo.vies - ajuste,
             )
         return modelo
 
+    def classificou_todos_corretamente(
+        self, exemplos: list[tuple[Classificação, Vetor[Pixel]]]
+    ) -> bool:
+        return all(
+            self.inferir(entrada) == classificacao
+            for classificacao, entrada in exemplos
+        )
+
 
 def main():
-    modelo = Modelo(pesos=[0] * 25, vies=0)
-    tentativas = 0
-    while (
-        any(
-            modelo.inferir(entrada) != classificacao
-            for classificacao, entrada in EXEMPLOS_Y
-        )
-        and tentativas < MAX_TENTATIVAS
-    ):
-        modelo = modelo.aprender(EXEMPLOS_Y)
-        tentativas += 1
-
-    if tentativas == MAX_TENTATIVAS:
+    taxa_aprendizado = 0.01
+    max_epocas = 10000
+    gerador = random.Random(123456789)
+    prob_flip = 2 / 25
+    num_exemplos_por_classificacao = 10
+    treinamento_y = gerar_exemplos_classificados(
+        num_exemplos_por_classificacao, gerador, prob_flip
+    )
+    num_pixels = len(treinamento_y[0][1])
+    modelo = Modelo(pesos=[0] * num_pixels, vies=0)
+    for _ in range(max_epocas):
+        modelo = modelo.aprender(taxa_aprendizado, treinamento_y)
+        if modelo.classificou_todos_corretamente(treinamento_y):
+            break
+    else:
         print("Não foi possível encontrar um modelo que classifique todos os exemplos.")
         return
-    print(modelo.pesos)
+    exemplos_nunca_vistos_y = gerar_exemplos_classificados(10_000, gerador, prob_flip)
+    acertos = sum(
+        modelo.inferir(exemplo) == classificacao
+        for classificacao, exemplo in exemplos_nunca_vistos_y
+    )
+    print(f"Acertos: {(100 * acertos / len(exemplos_nunca_vistos_y)):.2f}%")
+    printar_alguns_exemplos(exemplos_nunca_vistos_y, 5, gerador)
 
 
 if __name__ == "__main__":
